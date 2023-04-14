6d630291beb8d8fcaa55f80e92df9d3ca49e700a
Joe <joedeola@gmail.com>
2023-04-14 11:07:29 +0100

    Add files via upload

---
+++ SH.js
@@ -0,0 +1,4 @@
+// function Menu(e){
+//     const list = document.querySelector('ul');
+//     e.className === '<i class="fa-solid fa-bars"></i> ? (e.className = "<i class="fa-solid fa-xmark"></i>",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+// //   }
